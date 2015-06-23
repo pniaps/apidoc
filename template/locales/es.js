26d4af5a3f5dc1d7686cfcfcef7071789b220009
Pedro Peña <pedro.sanchez@smartsoft.es>
2015-06-23 15:01:02 +0200

    Add Spanish Translation

---
+++ template/locales/es.js
@@ -0,0 +1,25 @@
+define({
+    es: {
+        'Allowed values:'             : 'Valores permitidos:',
+        'Compare all with predecessor': 'Comparar todo con lo anterior',
+        'compare changes to:'         : 'comparar con:',
+        'compared to'                 : 'comparado con',
+        'Default value:'              : 'Valor por defecto:',
+        'Description'                 : 'Descripción',
+        'Field'                       : 'Campo',
+        'General'                     : 'General',
+        'Generated with'              : 'Generado con',
+        'Name'                        : 'Nombre',
+        'No response values.'         : 'Valores sin respuesta.',
+        'optional'                    : 'opcional',
+        'Parameter'                   : 'Parámetro',
+        'Permission:'                 : 'Permiso:',
+        'Response'                    : 'Respuesta',
+        'Send'                        : 'Enviar',
+        'Send a Sample Request'       : 'Envía una petición',
+        'show up to version:'         : 'mostrar has la versión:',
+        'Size range:'                 : 'Rango de tamaño:',
+        'Type'                        : 'Tipo',
+        'url'                         : 'url'
+    }
+});
